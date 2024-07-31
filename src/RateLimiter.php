ce7705033bf3947428ab07d30bf94b97c2aa5986
Khanh Duy Nguyen <khanhpkvn@gmail.com>
2024-07-31 11:42:56 +0700

    KDN-PC-01: Update

--- src/RateLimiter.php
+++ src/RateLimiter.php
@@ -16,11 +16,11 @@ class RateLimiter extends BaseInstances {
 	/**
 	 * @var DoctrineDbalAdapter|FilesystemAdapter|MemcachedAdapter|RedisAdapter|null $adapter
 	 */
-	protected mixed   $adapter          = null;
-	protected ?array  $limiters         = null;
-	protected ?string $key              = null;
-	protected ?string $store            = null;
-	protected ?array  $connectionParams = null;
+	protected $adapter          = null;
+	protected $limiters         = null;
+	protected $key              = null;
+	protected $store            = null;
+	protected $connectionParams = null;
 
 	/*
 	 *
